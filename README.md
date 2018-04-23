8236adfa05ec1dc162cf871a79e7718994e461b6
Giovanni Macciocu <x@z.local>
2018-04-24 00:18:27 +0200

    upd

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 backend requirements:
 
 $ pip install requests requests_oautlib websocket-server
+
+frontend:
+
+https://developers.google.com/maps/documentation/embed/get-api-key#key-restrictions
---
+++ frontend/web/app.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    //coordWin = new CoordWin('win-coord');
+    //twitterStreamWin = new TwitterStreamWin('win-twitterStream');
+
+
+    //coordWin.doc();
+    //twitterStream.doc();
+}
