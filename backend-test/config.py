37372c0bbf19f16294a12ee0a732ee080e26c1b5
giovanni.macciocu <giovanni.macciocu@nuance.com>
2018-04-25 20:20:11 +0200

    upd

--- backend-test/config.py
+++ backend-test/config.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
-CREDENTIALS = {
+CONFIG = {
     'CONSUMER_KEY': '77sZIhtpLTmykLRkIyOTebf7V',
     'CONSUMER_SECRET': 'UP7CvZHinRCj4IyYfC6vmrH187pPIQQ5CD3CJIwde4f5wydWR1',
     'ACCESS_TOK': '570855470-qwZHnkeeLL9D0JHPUgkfx8czIxBpRNVB6COFGrgW',
-    'ACCESS_TOK_SECRET': 'Px3qOO1js9icEmwqcGGc0q1izDCUMg88PrSD5vuv6sPLL'
+    'ACCESS_TOK_SECRET': 'Px3qOO1js9icEmwqcGGc0q1izDCUMg88PrSD5vuv6sPLL',
+    'SERVER_CONNECT_PATH': '/tmp/geotwitterstream_server.out'
 }
