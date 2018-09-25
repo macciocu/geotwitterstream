e0ac734ecab64a3103c7583a87f956638f55902b
Giovanni Macciocu <x@z.local>
2018-09-26 00:11:20 +0200

    initial commit

--- backend/geotwitterstream/config.py
+++ backend/geotwitterstream/config.py
@@ -6,10 +6,10 @@ date: Tue May  1 07:08:25 2018
 
 CONFIG = {
     # twitter application credentials
-    'CONSUMER_KEY': '77sZIhtpLTmykLRkIyOTebf7V',
-    'CONSUMER_SECRET': 'UP7CvZHinRCj4IyYfC6vmrH187pPIQQ5CD3CJIwde4f5wydWR1',
-    'ACCESS_TOK': '570855470-qwZHnkeeLL9D0JHPUgkfx8czIxBpRNVB6COFGrgW',
-    'ACCESS_TOK_SECRET': 'Px3qOO1js9icEmwqcGGc0q1izDCUMg88PrSD5vuv6sPLL',
+    'CONSUMER_KEY': '...',
+    'CONSUMER_SECRET': '...',
+    'ACCESS_TOK': '...'
+    'ACCESS_TOK_SECRET': '...',
     # websocket server address
     'SERVER_HOST': 'localhost',
     'SERVER_PORT': 2000
